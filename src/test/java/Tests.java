5ff45625c74514fede9af8417017b96a3e92c936
yaninapavlyk <yana.yankee@gmail.com>
2019-05-10 21:54:25 +0300

    apk

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -23,7 +23,8 @@ public class Tests  extends Helpers {
         // capabilities.setCapability("automationName", "Appium"); Original error: Could not find a driver for automationName 'Appium' and platformName 'Android'. Please check your desired capabilities.
         capabilities.setCapability("appPackage", "com.yoopies.babysittingandroid.beta");
         capabilities.setCapability("appActivity", "com.yoopies.loginmodule.activities.splash.SplashActivity");
-        capabilities.setCapability("app", "/Users/mobileteam/.jenkins/workspace/AndroidTestJob/apks");
+        capabilities.setCapability("app", "/Users/mobileteam/.jenkins/workspace/AndroidTestJob/apks/app-beta-debug.apk");
+
 
         driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
